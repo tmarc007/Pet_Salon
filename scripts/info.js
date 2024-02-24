0902597e117bc407463497472d3d2f316005a5e6
Tmarc007 <thomasmarcello@gmail.com>
2024-02-23 16:53:35 -0800

    Pet Salon_105_final

--- scripts/info.js
+++ scripts/info.js
@@ -1,16 +1,18 @@
 let salon={//object
-    name:"The fashion pet",
+    name:"🐶 The Fashion Pet 🐶",
     phone:"999-999-9999",
     address:{
-        street:"Palm",
-        number:"262-K",
-        zip:"22333"
+        number:"262",
+        street:"Heaven St.",
+        city:"San Diego,",
+        state:"California",
+        zip:"92145"
     },
     pets:[] //array
 }
 function displayFooterInfo(){
     document.getElementById("info").innerHTML=`
-    <p> Welcome to the ${salon.name} is located in ${salon.address.street} ${salon.address.number} ${salon.address.zip}</p>
+    <p> Welcome to the ${salon.name} We are located at: ${salon.address.number} ${salon.address.street}  ${salon.address.city} ${salon.address.state} ${salon.address.zip}</p>
     `;
 }
 
